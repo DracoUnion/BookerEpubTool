8c59b0ceb9caa34b0710f45d9453de5e0bb2a305
wizardforcel <562826179@qq.com>
2023-07-09 23:05:50 +0800

    2023-07-09 23:05:50

--- BookerEpubTool/toc.py
+++ BookerEpubTool/toc.py
@@ -5,6 +5,7 @@ from EpubCrawler.util import is_pic, safe_mkdir, safe_rmdir
 import subprocess as subp
 from pyquery import PyQuery as pq
 import re
+from .util import *
 
 def get_opf_flist(cont_opf):
     cont_opf = re.sub(r'<\?xml[^>]*\?>', '', cont_opf)
@@ -73,13 +74,12 @@ def get_toc(args):
         print('请提供 EPUB 文件')
         return
         
-    bio = BytesIO(open(fname, 'rb').read())
-    zip = zipfile.ZipFile(bio, 'r', zipfile.ZIP_DEFLATED)
-    ncx_fname, _ = get_toc_and_content_path(zip)
+    fdict = read_zip(fname)
+    ncx_fname, _ = get_toc_and_content_path(fdict)
     if ncx_fname is None:
         print('未找到目录文件 toc.ncx')
         return
-    toc_ncx = zip.read(ncx_fname).decode('utf8')
+    toc_ncx = fdict[ncx_fname].decode('utf8')
     toc = get_ncx_toc(toc_ncx, args.regex, args.hlevel)
     for i, ch in enumerate(toc):
         pref = '>' * (ch["level"] - 1)
@@ -91,22 +91,22 @@ def get_html_body(html):
     rt = pq(html)
     return rt('body').html() if rt('body') else html
 
-def get_toc_and_content_path(zip):
+def get_toc_and_content_path(fdict):
     meta_path = 'META-INF/container.xml'
-    if meta_path not in zip.namelist():
+    if meta_path not in fdict:
         return (None, None)
-    meta = zip.read(meta_path).decode('utf-8')
+    meta = fdict[meta_path].decode('utf-8')
     meta = re.sub(r'<\?xml[^>]*\?>', '', meta)
     meta = re.sub(r'xmlns=".+?"', '', meta)
     opf_path = pq(meta).find('rootfile').attr('full-path') or ''
-    if opf_path not in zip.namelist():
+    if opf_path not in fdict:
         return (None, None)
-    opf = zip.read(opf_path).decode('utf-8')
+    opf = fdict[opf_path].decode('utf-8')
     opf = re.sub(r'<\?xml[^>]*\?>', '', opf)
     opf = re.sub(r'xmlns=".+?"', '', opf)
     ncx_path = pq(opf).find('item#ncx').attr('href') or ''
     ncx_path = path.join(path.dirname(opf_path), ncx_path).replace('\\', '/')
-    if ncx_path not in zip.namelist():
+    if ncx_path not in fdict:
         return (None, None)
     return (ncx_path, opf_path)
             
@@ -126,14 +126,13 @@ def ext_chs(args):
         return
 
     # 获取目录和文件列表
-    bio = BytesIO(open(fname, 'rb').read())
-    zip = zipfile.ZipFile(bio, 'r', zipfile.ZIP_DEFLATED)
-    ncx_fname, opf_fname = get_toc_and_content_path(zip)
+    fdict = read_zip(fname)
+    ncx_fname, opf_fname = get_toc_and_content_path(fdict)
     if ncx_fname is None:
         print('未找到目录文件 toc.ncx')
         return
-    toc_ncx = zip.read(ncx_fname).decode('utf8')
-    cont_opf = zip.read(opf_fname).decode('utf8')
+    toc_ncx = fdict[ncx_fname].decode('utf8')
+    cont_opf = fdict[opf_fname].decode('utf8')
     toc = get_ncx_toc(toc_ncx, rgx, hlv)
     flist = get_opf_flist(cont_opf)
     toc_flist = {
@@ -144,9 +143,9 @@ def ext_chs(args):
     book_dir = path.dirname(opf_fname)
     chs = []
     for f in flist:
-        cont = zip.read(
+        cont = fdict[
             path.join(book_dir, f).replace('\\', '/')
-        ).decode('utf8')
+        ].decode('utf8')
         cont = get_html_body(cont)
         if f in toc_flist:
             chs.append([cont])
