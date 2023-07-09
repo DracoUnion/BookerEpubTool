0d481a083954cd17412cd460045b6f8ea771088c
wizardforcel <562826179@qq.com>
2023-07-09 22:46:30 +0800

    2023-07-09 22:46:30

--- BookerEpubTool/util.py
+++ BookerEpubTool/util.py
@@ -1,5 +1,8 @@
 import zipfile
 from io import BytesIO
+import re
+import subprocess as subp
+from os import path
 
 def read_zip(fname):
     bio = BytesIO(open(fname, 'rb').read())
@@ -15,3 +18,12 @@ def write_zip(fname, fdict):
         zip.writestr(name, data)
     zip.close()
     open(fname, 'wb').write(bio.getvalue())
+
+def convert_to_epub(fname):
+    nfname = re.sub(r'\.\w+$', '', fname) + '.epub'
+    print(f'{fname} => {nfname}')
+    subp.Popen(f'ebook-convert "{fname}" "{nfname}"', 
+        shell=True, stdin=subp.PIPE, stdout=subp.PIPE).communicate()
+    if not path.exists(nfname):
+        raise FileNotFoundError(f'{nfname} not found')
+    return nfname
